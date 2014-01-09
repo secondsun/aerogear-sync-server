0bc50dc7cc0e500f9461d69de69f59f3d600444d
Daniel Bevenius <daniel.bevenius@gmail.com>
2014-01-09 16:54:51 +0100

    Adhering to aerogear-js code standards.

--- js-client/tests/sync/sync.js
+++ js-client/tests/sync/sync.js
@@ -4,19 +4,19 @@
 
     test('create document', function() {
         var documentId = uuid();
-        var response = putDocument(documentId, {model: 'honda'});
-        equal(response.status, 200, 'Status should be 200');
-        equal(response.doc.id, documentId, 'id should match the sent path id parameter.');
-        equal(response.doc.content.model, 'honda', 'model property should be honda');
+        var response = putDocument( documentId, {model: 'honda'} );
+        equal( response.status, 200, 'Status should be 200' );
+        equal( response.doc.id, documentId, 'id should match the sent path id parameter.' );
+        equal( response.doc.content.model, 'honda', 'model property should be honda' );
     });
 
     test('get document', function() {
         var documentId = uuid(),
             getDoc;
-        putDocument(documentId, {model: 'ferrari'});
-        getDoc = getDocument(documentId);
-        equal(getDoc.id, documentId, 'id should match the sent path id parameter.');
-        equal(getDoc.content.model, 'ferrari', 'id should match the sent path id parameter.');
+        putDocument( documentId, {model: 'ferrari'} );
+        getDoc = getDocument( documentId );
+        equal( getDoc.id, documentId, 'id should match the sent path id parameter.' );
+        equal( getDoc.content.model, 'ferrari', 'id should match the sent path id parameter.' );
     });
 
     test('update document', function() {
@@ -37,42 +37,44 @@
         notEqual( updatedDoc.doc.rev, getDoc.rev, 'revisions should be different' );
     });
 
-    function getDocument(id) {
-        var xhr = xhrObject('GET', id);
-        xhr.send(null);
-        console.log(xhr.responseText);
-        return Doc.fromJson(xhr.responseText);
+    function getDocument( id ) {
+        var xhr = xhrObject( 'GET', id );
+        xhr.send( null );
+        console.log( xhr.responseText );
+        return Doc.fromJson( xhr.responseText );
     };
 
-    function putDocument(id, content, rev) {
-        var xhr = xhrObject('PUT', id);
-        xhr.send(JSON.stringify({content: content, rev: rev}));
-        return {status: xhr.status, doc: Doc.fromJson(xhr.responseText)};
+    function putDocument( id, content, rev ) {
+        var xhr = xhrObject( 'PUT', id );
+        xhr.send( JSON.stringify( {content: content, rev: rev} ) );
+        return {status: xhr.status, doc: Doc.fromJson( xhr.responseText )};
     };
 
-    function httpRequest(method, id, content) {
+    function httpRequest( method, id, content ) {
         var xhr = xhrObject();
-        xhr.send(JSON.stringify({content: content}));
+        xhr.send( JSON.stringify( {content: content} ) );
         return xhr;
     };
 
-    function xhrObject(method, id) {
+    function xhrObject( method, id ) {
         var xhr = new XMLHttpRequest();
-        xhr.open(method, 'http://localhost:8080/' + id, false);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.open( method, 'http://localhost:8080/' + id, false );
+        xhr.setRequestHeader( 'Content-Type', 'application/json' );
+        xhr.setRequestHeader( 'Accept', 'application/json' );
         return xhr;
     };
 
-    function Doc(id, rev, content) {
+    function Doc( id, rev, content ) {
         this.id = id;
         this.rev = rev;
         this.content = content;
     };
-    Doc.fromJson = function(str) {
-        var json = JSON.parse(str);
-        return new Doc(json.id, json.rev, JSON.parse(json.content));
+
+    Doc.fromJson = function( str ) {
+        var json = JSON.parse( str );
+        return new Doc( json.id, json.rev, JSON.parse( json.content ) );
     }
+
     Doc.prototype = {
         toString: function() {
             return "[id=" + this.id + ", rev=" + this.rev + ", content=" + content + "]";
@@ -81,9 +83,9 @@
 
     function uuid()
     {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function( c ) {
             var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
-            return v.toString(16);
+            return v.toString( 16 );
         });
     }
 
