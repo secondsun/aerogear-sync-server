86d0735c9c86328fb00224576233a8c42d9b527f
Daniel Bevenius <daniel.bevenius@gmail.com>
2014-01-09 15:23:44 +0100

    Adding test to get a document.

--- js-client/tests/sync/sync.js
+++ js-client/tests/sync/sync.js
@@ -2,22 +2,63 @@
 
     module('Sync integration test');
 
-    test('create (PUT) document', function() {
+    test('create document', function() {
         var documentId = uuid();
-        var response = httpRequest('PUT', documentId, 'honda');
+        var response = putDocument(documentId, {model: 'honda'});
         equal(response.status, 200, 'Status should be 200');
-        equal(response.id, documentId, 'id should match the sent path id parameter.');
-        equal(response.content.model, 'honda', 'model property should be honda');
+        equal(response.doc.id, documentId, 'id should match the sent path id parameter.');
+        equal(response.doc.content.model, 'honda', 'model property should be honda');
     });
 
-    function httpRequest(method, id, make) {
-        var req = new XMLHttpRequest();
-        req.open(method, 'http://localhost:8080/' + id, false);
-        req.setRequestHeader('Content-Type', 'application/json');
-        req.send(JSON.stringify({content: {model: make}}));
-        var json = JSON.parse(req.responseText);
-        return {status: req.status, id: json.id, rev: json.rev, content: JSON.parse(json.content)};
+    test('get document', function() {
+        var documentId = uuid();
+        putDocument(documentId, {model: 'ferrari'});
+        var doc = getDocument(documentId);
+        equal(doc.id, documentId, 'id should match the sent path id parameter.');
+        equal(doc.content.model, 'ferrari', 'id should match the sent path id parameter.');
+    });
+
+    function getDocument(id) {
+        var xhr = xhrObject('GET', id);
+        xhr.send(null);
+        console.log(xhr.responseText);
+        return Doc.fromJson(xhr.responseText);
     };
+
+    function putDocument(id, content, rev) {
+        var xhr = xhrObject('PUT', id);
+        xhr.send(JSON.stringify({content: content}));
+        return {status: xhr.status, doc: Doc.fromJson(xhr.responseText)};
+    };
+
+    function httpRequest(method, id, content) {
+        var xhr = xhrObject();
+        xhr.send(JSON.stringify({content: content}));
+        return xhr;
+    };
+
+    function xhrObject(method, id) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, 'http://localhost:8080/' + id, false);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Accept', 'application/json');
+        return xhr;
+    };
+
+    function Doc(id, rev, content) {
+        this.id = id;
+        this.rev = rev;
+        this.content = content;
+    };
+    Doc.fromJson = function(str) {
+        var json = JSON.parse(str);
+        return new Doc(json.id, json.rev, JSON.parse(json.content));
+    }
+    Doc.prototype = {
+        toString: function() { 
+            return "[id=" + this.id + ", rev=" + this.rev + ", content=" + content + "]"; 
+        }
+    }
 
     function uuid()
     {
