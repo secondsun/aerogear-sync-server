13368d264b9d894e8e26e1a22a371d1435d62e26
Summers Pittman <secondsun@gmail.com>
2014-10-13 15:47:32 -0400

    Initial "it compiles" commit

--- diffsync/client-xmpp/src/main/java/org/jboss/aerogear/diffsync/DiffSyncClientHandler.java
+++ diffsync/client-xmpp/src/main/java/org/jboss/aerogear/diffsync/DiffSyncClientHandler.java
@@ -1,29 +1,32 @@
 /**
- * JBoss, Home of Professional Open Source
- * Copyright Red Hat, Inc., and individual contributors.
+ * JBoss, Home of Professional Open Source Copyright Red Hat, Inc., and
+ * individual contributors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- * 	http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.jboss.aerogear.diffsync;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 import org.jboss.aerogear.diffsync.client.ClientSyncEngine;
-import org.jboss.aerogear.diffsync.server.MessageType;
 
+public class DiffSyncClientHandler extends BroadcastReceiver {
 
-public class DiffSyncClientHandler {
-
-    private static final Logger logger = LoggerFactory.getLogger(DiffSyncClientHandler.class);
+    private final String TAG = DiffSyncClientHandler.class.getSimpleName();
 
     private final ClientSyncEngine<?> syncEngine;
 
@@ -31,46 +34,54 @@ public class DiffSyncClientHandler {
         this.syncEngine = syncEngine;
     }
 
-    protected void messageReceived(final ChannelHandlerContext ctx, final WebSocketFrame frame) throws Exception {
-        if (frame instanceof CloseWebSocketFrame) {
-            logger.debug("Received closeFrame");
-            ctx.close();
-            return;
-        }
-
-        if (frame instanceof TextWebSocketFrame) {
-            logger.info("TextWebSocketFrame: " + ((TextWebSocketFrame)frame).text());
-            final JsonNode json = JsonMapper.asJsonNode(((TextWebSocketFrame) frame).text());
-            logger.info("json: " + json);
-            switch (MessageType.from(json.get("msgType").asText())) {
-            case PATCH:
-                final PatchMessage serverPatchMessage = JsonMapper.fromJson(json.toString(), DefaultPatchMessage.class);
-                logger.info("Edits: " + serverPatchMessage);
-                patch(serverPatchMessage);
-                break;
-            case UNKNOWN:
-                unknownMessageType(ctx, json);
-                break;
-            }
-        } else {
-            ctx.fireChannelRead(frame);
-        }
-    }
 
     private void patch(final PatchMessage clientEdit) {
         syncEngine.patch(clientEdit);
     }
 
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        logger.error("Caught exception", cause);
-    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
+        // The getMessageType() intent parameter must be the intent you received
+        // in your BroadcastReceiver.
+        String messageType = gcm.getMessageType(intent);
 
-    private static void unknownMessageType(final ChannelHandlerContext ctx, final JsonNode json) {
-        ctx.channel().writeAndFlush(textFrame("{\"result\": \"Unknown msgType '" + json.get("msgType").asText() + "'\"}"));
-    }
+        if (!extras.isEmpty()) {  // has effect of unparcelling Bundle
+            /*
+             * Filter messages based on message type. Since it is likely that GCM
+             * will be extended in the future with new message types, just ignore
+             * any message types you're not interested in, or that you don't
+             * recognize.
+             */
+            if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+                Log.i(TAG, "Send error: " + extras.toString());
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
+                Log.i(TAG, "Deleted messages on server: "
+                        + extras.toString());
+                // If it's a regular GCM message, do some work.
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
 
-    private static TextWebSocketFrame textFrame(final String text) {
-        return new TextWebSocketFrame(text);
+                /*
+                Log.i(TAG, "TextWebSocketFrame: " + ((TextWebSocketFrame) frame).text());
+            final JsonNode json = JsonMapper.asJsonNode(((TextWebSocketFrame) frame).text());
+            Log.i(TAG, "json: " + json);
+            switch (MessageType.from(json.get("msgType").asText())) {
+                case PATCH:
+                    final PatchMessage serverPatchMessage = JsonMapper.fromJson(json.toString(), DefaultPatchMessage.class);
+                    Log.i(TAG, "Edits: " + serverPatchMessage);
+                    patch(serverPatchMessage);
+                    break;
+                case UNKNOWN:
+                    unknownMessageType(ctx, json);
+                    break;
+            }
+                */
+                
+                Log.i(TAG, "Received: " + extras.toString());
+            }
+        }
+
     }
 
 }
